0b66299d538bd4e5769e80a3058ea49c209e6b89
Alejandro <alejandro.devop@gmail.com>
2020-10-17 22:34:32 -0500

    Finished the lesson 18, post forms

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,13 @@
 @extends('layout')
 @section('title', 'Contact')
 @section('content')
-<h1>Contact</h1>
+	<h1>Contact</h1>
+	<form method="post" action="{{ route('contact') }}">
+		@csrf
+		<input name="name" type="text" placeholder="Nombre"><br />
+		<input name="subject" type="text" placeholder="Subject"><br />
+		<input name="email" type="text" placeholder="Email"><br />
+		<textarea name="content"></textarea><br />
+		<button type="submit">Enviar</button>
+	</form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::get('/portfolio', 'App\\Http\\Controllers\\PortfolioController@index' )->name('portfolio');
+Route::post('/contact', 'App\\Http\\Controllers\\MessagesController@store')->name('contact');
 // Route::resource('portfolio', 'App\\Http\\Controllers\\PortfolioController');
